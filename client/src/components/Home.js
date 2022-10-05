b53d09f2d6236174b67f3c42dbc2c82475af14c3
Olivia <oliviatmanalastas@gmail.com>
2022-10-05 19:02:33 -0400

    Added css styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,9 @@
+import '../styles/Home.css'
 
 const Home = () => {
    return (
-      <div>
-         <p> Home </p>
+      <div className='home'>
+         <h1 style={{ fontSize: "3.5em", fontFamily: "Caveat" }} className='transfer-animation'> QR <span className='transfer-animation'> </span></h1>
       </div>
    )
 }
